d338acbfaf9a9f7f18272ff9486b3e4731f7f031
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-20 12:53:23 +0300

    Add edit and update actions and views to Projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,8 @@
 class ProjectsController < ApplicationController
+  before_action :set_projects, only: [:index, :edit]
+  before_action :set_project, except: [:index, :create]
+
   def index
-    @projects = current_user.projects
   end
 
   def create
@@ -11,12 +13,40 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def edit
+    respond_to do |format|
+      format.html { render 'index' }
+      format.js
+    end
+  end
+
+  def update
+    @project.update_attributes(project_params)
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
+  end
+
   def destroy
-    @project = current_user.projects.find(params[:id])
     @project.destroy
     respond_to do |format|
       format.html { redirect_to projects_path }
       format.js
     end
+  end
+
+  private
+
+  def set_project
+    @project = current_user.projects.find(params[:id])
+  end
+
+  def set_projects
+    @projects = current_user.projects
+  end
+
+  def project_params
+    params.require(:project).permit(:name)
   end
 end
