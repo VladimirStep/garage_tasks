6b40f0020e4d0c6445aa3fc4887d13b81688dbd3
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-20 16:12:28 +0300

    Add Tasks controller

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :project, counter_cache: true
-  
+
   validates :name, presence: true, length: { in: 2..50 }
   validates :priority, presence: true, numericality: { only_integer: true }
   validates :status, presence: true, inclusion: { in: %w(new in-progress completed) }
