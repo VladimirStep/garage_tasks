9676e562aac9eb1bd2e5c1a965338d4546c7ae5f
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-20 17:21:03 +0300

    Add action to create new task. Start designing task partial.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_project
+
   def create
+    @task = @project.tasks.create(task_params)
   end
 
   def edit
@@ -9,5 +12,15 @@ class TasksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def set_project
+    @project = current_user.projects.find(params[:project_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,13 @@ class Task < ApplicationRecord
   belongs_to :project, counter_cache: true
 
   validates :name, presence: true, length: { in: 2..50 }
-  validates :priority, presence: true, numericality: { only_integer: true }
-  validates :status, presence: true, inclusion: { in: %w(new in-progress completed) }
+  validates :priority, presence: true, numericality: { only_integer: true }, on: :update
+  validates :status, presence: true, inclusion: { in: %w(new in-progress completed) }, on: :update
+
+  before_create :set_defaults
+
+  def set_defaults
+    self.status = 'new'
+    self.priority = self.project.tasks.count
+  end
 end
