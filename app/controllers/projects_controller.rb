f6321d30b495215df6eacde1d892981de8e60466
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-20 10:59:13 +0300

    Add create, destroy actions for projects by ajax

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,22 @@
 class ProjectsController < ApplicationController
   def index
+    @projects = current_user.projects
+  end
+
+  def create
+    @project = current_user.projects.create(name: 'New Project')
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
+  end
+
+  def destroy
+    @project = current_user.projects.find(params[:id])
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
   end
 end
