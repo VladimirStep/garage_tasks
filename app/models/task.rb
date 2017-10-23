cf7795a6e0528657d8eb16f7cd8b359c041b4bf3
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-23 17:16:53 +0300

    Show tasks by priority. Apdate priority on delete.

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,9 +6,16 @@ class Task < ApplicationRecord
   validates :status, presence: true, inclusion: { in: %w(new in-progress completed) }, on: :update
 
   before_create :set_defaults
+  after_destroy :update_priority
 
   def set_defaults
     self.status = 'new'
     self.priority = self.project.tasks.count
+  end
+
+  def update_priority
+    self.project&.tasks.find_each do |task|
+      task.update_attribute(:priority, task.priority -= 1)
+    end
   end
 end
