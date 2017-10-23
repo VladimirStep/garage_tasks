8c33a37e894a952eaf78d10cf865751112aeaf30
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-23 10:23:38 +0300

    Destroy task by ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_project
+  before_action :set_task, except: [:create]
 
   def create
     @task = @project.tasks.create(task_params)
@@ -16,12 +17,21 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_path }
+      format.js
+    end
   end
 
   private
 
   def set_project
     @project = current_user.projects.find(params[:project_id])
+  end
+
+  def set_task
+    @task = @project.tasks.find(params[:id])
   end
 
   def task_params
