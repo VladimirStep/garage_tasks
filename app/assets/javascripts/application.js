4142b1b3c66611266b5bdd3b6ebd2e605df61571
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-24 16:50:08 +0300

    Add client validation to new task form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,4 @@
 //= require moment
 //= require bootstrap-datetimepicker
 //= require jquery.validate
+//= require projects
--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,14 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on("turbolinks:load", function(){
+    $('.new-task form').each(function () {
+        $(this).validate({
+            submitHandler: function(form){
+                $(form).trigger('submit.rails');
+                $('input.task-name', form).val('');
+            }
+        });
+        $(this).focusout(function(){
+            $('input.task-name', $(this)).val('');
+            $(this).validate().resetForm();
+        });
+    });
+});
