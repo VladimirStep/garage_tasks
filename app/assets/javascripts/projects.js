7865fc6e8eb0be1ba2bee56da1373617ba0cde53
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-25 10:01:26 +0300

    Add cliend side validation to new created project's new task form

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,14 +1,22 @@
 $(document).on("turbolinks:load", function(){
     $('.new-task form').each(function () {
-        $(this).validate({
+        var current_form = $(this);
+        var input_task_field  = $('input.task-name', $(this));
+
+        current_form.validate({
+            onfocusout: false,
             submitHandler: function(form){
-                $(form).trigger('submit.rails');
-                $('input.task-name', form).val('');
+                $(form).trigger('submit.rails')
             }
         });
-        $(this).focusout(function(){
-            $('input.task-name', $(this)).val('');
-            $(this).validate().resetForm();
+
+        input_task_field.focusout(function(){
+            $(this).val('');
+            current_form.validate().resetForm();
+        });
+
+        current_form.on('ajax:complete', function(){
+            input_task_field.val('');
         });
     });
 });
