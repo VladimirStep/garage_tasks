6048a94edf2445193f1da9b9a45911094b702146
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-10-31 10:46:10 +0200

    Add flash norices to views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def set_flash_class(key)
+    case key
+      when 'alert'
+        'red-gradient'
+      when 'notice'
+        'green-gradient'
+      else
+        key
+    end
+  end
 end
