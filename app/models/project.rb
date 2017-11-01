284449507916260832085f05693010704f548c48
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-01 14:39:18 +0200

    Refactor models using style guide

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,17 +5,21 @@ class Project < ApplicationRecord
   validates :name, presence: true, length: { in: 2..50 }
 
   scope :tasks_count_desc, -> { order(tasks_count: :desc).pluck(:tasks_count) }
-  scope :tasks_count_order_by_project_name, -> { order(:name).pluck(:tasks_count) }
-  scope :containing_letter_shows_tasks_count, -> (char) { where('name LIKE ?', ('%' + char + '%')).pluck(:name, :tasks_count) }
-  scope :over_ten_completed_tasks, -> { joins(:tasks)
-                                            .where(tasks: { status: 'completed' })
-                                            .group(:name)
-                                            .having('COUNT(*) > ?', 10)
-                                            .order(:id)
-                                            .count
-                                            .to_a }
-  
+  scope :tasks_count_order_by_project_name,
+        -> { order(:name).pluck(:tasks_count) }
+  scope :containing_letter_shows_tasks_count,
+        lambda { |char|
+          where('name LIKE ?', ('%' + char + '%')).pluck(:name, :tasks_count)
+        }
+  scope :over_ten_completed_tasks,
+        lambda {
+          joins(:tasks).where(tasks: { status: 'completed' }).group(:name)
+                       .having('COUNT(*) > ?', 10).order(:id).count.to_a
+        }
+
   def reorder_tasks(new_order)
-    new_order.each_with_index {|id, index| self.tasks&.find(id.to_i)&.set_list_position(index + 1)}
+    new_order.each_with_index do |id, index|
+      tasks&.find(id.to_i)&.set_list_position(index + 1)
+    end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -5,31 +5,36 @@ class Task < ApplicationRecord
   acts_as_list scope: :project, column: :priority
 
   validates :name, presence: true, length: { in: 2..50 }
-  validates :priority, presence: true, numericality: { only_integer: true }, on: :update
+  validates :priority, presence: true,
+                       numericality: { only_integer: true },
+                       on: :update
   validates :status, presence: true, on: :update
 
   before_create :set_defaults
   before_destroy :check_status
 
   scope :statuses_ordered_asc, -> { statuses.keys.sort }
-  scope :begins_with_letter, -> (char) { where('name LIKE ?', (char + '%')) }
-  scope :with_duplicate_names, -> { select(:name).group(:name).having('COUNT(*) > ?', 1).order(:name).pluck(:name) }
-  scope :exact_match_from_project, -> (project_name) { joins(:project)
-                                                           .where('projects.name = ?', project_name)
-                                                           .group(:name, :status)
-                                                           .having('COUNT(*) > ?', 1)
-                                                           .order('COUNT(*)')
-                                                           .count
-                                                           .to_a }
+  scope :begins_with_letter, ->(char) { where('name LIKE ?', (char + '%')) }
+  scope :with_duplicate_names,
+        lambda {
+          select(:name).group(:name).having('COUNT(*) > ?', 1)
+                       .order(:name).pluck(:name)
+        }
+  scope :exact_match_from_project,
+        lambda { |project_name|
+          joins(:project).where('projects.name = ?', project_name)
+                         .group(:name, :status).having('COUNT(*) > ?', 1)
+                         .order('COUNT(*)').count.to_a
+        }
 
   def set_defaults
     self.status = 'new task'
   end
 
   def check_status
-    if self.status == 'in-progress'
-      errors.add(:base, :task_is_not_completed, message: 'Task can not be deleted until it is in progress')
-      throw(:abort)
-    end
+    return unless status == 'in-progress'
+    errors.add(:base, :task_is_not_completed,
+               message: 'Task can not be deleted until it is in progress')
+    throw(:abort)
   end
 end
