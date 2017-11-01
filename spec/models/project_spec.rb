dd109c12fad35db346b1f4eea69926afadc5b9cf
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-01 16:00:45 +0200

    Refactor spec tests using style guide

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
@@ -22,7 +24,8 @@ RSpec.describe Project, type: :model do
   end
 
   describe 'scopes' do
-    it '.tasks_count_desc get the count fo all tasks in each project, order by tasks count desc' do
+    it '.tasks_count_desc get the count fo all tasks in each project,'\
+        ' order by tasks count desc' do
       project1 = create(:project)
       project2 = create(:project)
       project3 = create(:project)
@@ -34,7 +37,8 @@ RSpec.describe Project, type: :model do
       expect(Project.tasks_count_desc).to eq([9,5,2])
     end
 
-    it '.tasks_count_order_by_project_name get the count fo all tasks in each project, order by project name' do
+    it '.tasks_count_order_by_project_name get the count fo all tasks'\
+        ' in each project, order by project name' do
       project1 = create(:project, name: 'BBB')
       project2 = create(:project, name: 'AAA')
       project3 = create(:project, name: 'CCC')
@@ -43,10 +47,12 @@ RSpec.describe Project, type: :model do
       5.times { create(:task, project: project2) }
       9.times { create(:task, project: project3) }
 
-      expect(Project.tasks_count_order_by_project_name).to eq([5,2,9])
+      expect(Project.tasks_count_order_by_project_name).to eq([5, 2, 9])
     end
 
-    it '.containing_letter_shows_tasks_count get the list of all projects containing the letter "x" in the middle of the name, and show the tasks count near each project' do
+    it '.containing_letter_shows_tasks_count get the list of all projects'\
+        ' containing the letter "x" in the middle of the name,'\
+        ' and show the tasks count near each project' do
       project1 = create(:project, name: 'ABCD')
       project2 = create(:project, name: 'EFGH')
       project3 = create(:project, name: 'ACFH')
@@ -55,10 +61,13 @@ RSpec.describe Project, type: :model do
       5.times { create(:task, project: project2) }
       9.times { create(:task, project: project3) }
 
-      expect(Project.containing_letter_shows_tasks_count('C')).to contain_exactly(['ABCD', 2], ['ACFH', 9])
+      expect(Project.containing_letter_shows_tasks_count('C'))
+        .to contain_exactly(['ABCD', 2], ['ACFH', 9])
     end
 
-    it '.over_ten_completed_tasks get the list of project names having more then 10 tasks in status "completed", order by project_id' do
+    it '.over_ten_completed_tasks get the list of project names'\
+        ' having more then 10 tasks in status "completed",'\
+        ' order by project_id' do
       project1 = create(:project, name: 'Project 1')
       project2 = create(:project, name: 'Project 2')
       project3 = create(:project, name: 'Project 3')
@@ -71,11 +80,13 @@ RSpec.describe Project, type: :model do
       project2_task_ids = project2.tasks.pluck(:id).sample(8)
       project3_task_ids = project3.tasks.pluck(:id).sample(11)
 
-      sample_ids = project1_task_ids.concat(project2_task_ids).concat(project3_task_ids)
+      sample_ids = project1_task_ids.concat(project2_task_ids)
+                                    .concat(project3_task_ids)
 
       Task.where(id: sample_ids).update_all(status: 'completed')
 
-      expect(Project.over_ten_completed_tasks).to eq([[project1.name, 16], [project3.name, 11]])
+      expect(Project.over_ten_completed_tasks)
+        .to eq([[project1.name, 16], [project3.name, 11]])
     end
   end
 
@@ -92,7 +103,7 @@ RSpec.describe Project, type: :model do
         expect(old_order).to eq(project.tasks.order(:id).pluck(:id))
 
         new_order_ids = project.tasks.ids.shuffle
-        project.reorder_tasks(new_order_ids.map{ |id| id.to_s })
+        project.reorder_tasks(new_order_ids.map(&:to_s))
 
         new_order = project.tasks.order(:priority).pluck(:id)
 
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
@@ -23,7 +25,8 @@ RSpec.describe Task, type: :model do
   end
 
   describe 'scopes' do
-    it '.statuses_ordered_asc get all statuses, not repeating, alphabetically ordered' do
+    it '.statuses_ordered_asc get all statuses, not repeating,'\
+        ' alphabetically ordered' do
       20.times { create(:task) }
 
       all_ids = Task.ids
@@ -33,19 +36,23 @@ RSpec.describe Task, type: :model do
       Task.where(id: ids_for_completed).update_all(status: 'completed')
       Task.where(id: ids_for_inprogress).update_all(status: 'in-progress')
 
-      expect(Task.statuses_ordered_asc).to eq(['completed', 'in-progress', 'new task'])
+      expect(Task.statuses_ordered_asc)
+        .to eq(['completed', 'in-progress', 'new task'])
     end
 
-    it '.begins_with_letter get the tasks for all projects having the name beginning with "x" letter' do
+    it '.begins_with_letter get the tasks for all projects'\
+        ' having the name beginning with "x" letter' do
       20.times { create(:task) }
 
       sample_task_ids = Task.ids.sample(5)
       Task.where(id: sample_task_ids).update_all(name: 'My task')
 
-      expect(Task.begins_with_letter('M')).to match_array(Task.find(sample_task_ids))
+      expect(Task.begins_with_letter('M'))
+        .to match_array(Task.find(sample_task_ids))
     end
 
-    it '.with_duplicate_names get the list of tasks with duplicate names, order alphabetically' do
+    it '.with_duplicate_names get the list of tasks with duplicate names,'\
+        ' order alphabetically' do
       20.times { create(:task) }
 
       all_ids = Task.ids
@@ -57,10 +64,13 @@ RSpec.describe Task, type: :model do
       Task.where(id: ids_for_dup2).update_all(name: 'Task name 1')
       Task.where(id: ids_for_dup3).update_all(name: 'Task name 2')
 
-      expect(Task.with_duplicate_names).to eq(['Task name 1', 'Task name 2', 'Task name 3'])
+      expect(Task.with_duplicate_names)
+        .to eq(['Task name 1', 'Task name 2', 'Task name 3'])
     end
 
-    it '.exact_match_from_project get the list of tasks having several exact matches of both name and status from the project "x" order by matches count' do
+    it '.exact_match_from_project get the list of tasks having several'\
+        ' exact matches of both name and status from the project "x"'\
+        ' order by matches count' do
       project1 = create(:project, name: 'Project 1')
       project2 = create(:project, name: 'Project 2')
       project3 = create(:project, name: 'Garage')
@@ -74,11 +84,17 @@ RSpec.describe Task, type: :model do
         ids_1 = all_ids.sample(4)
         ids_2 = (all_ids - ids_1).sample(2)
 
-        Task.where(id: ids_1).update_all(name: 'Task name 1', status: 'in-progress')
-        Task.where(id: ids_2).update_all(name: 'Task name 2', status: 'completed')
+        Task.where(id: ids_1).update_all(name: 'Task name 1',
+                                         status: 'in-progress')
+        Task.where(id: ids_2).update_all(name: 'Task name 2',
+                                         status: 'completed')
       end
 
-      expect(Task.exact_match_from_project('Garage')).to eq([[['Task name 2', 'completed'], 2], [['Task name 1', 'in-progress'], 4]])
+      expect(Task.exact_match_from_project('Garage'))
+        .to eq([
+                 [['Task name 2', 'completed'], 2],
+                 [['Task name 1', 'in-progress'], 4]
+               ])
     end
   end
 end
