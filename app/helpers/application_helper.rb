0fbe122fa248c7971152435cbd56545d4567ecd2
Vladimir Stepanov <vladimir.ibox@gmail.com>
2017-11-01 15:15:38 +0200

    Refactor helpers using style guide

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  def set_flash_class(key)
+  def flash_class(key)
     case key
-      when 'alert'
-        'red-gradient'
-      when 'notice'
-        'green-gradient'
-      else
-        key
+    when 'alert'
+      'red-gradient'
+    when 'notice'
+      'green-gradient'
+    else
+      key
     end
   end
 end
